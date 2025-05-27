d7e3dfe1284efd826918457a7549cbd47238a90c
VinhNV-06 <vinhnv.ptit@gmail.com>
2025-05-27 20:32:17 +0700

    bai tap day 11

---
+++ Day11_2025_05_27/mainfest.js
@@ -0,0 +1,7 @@
+document.title = "Chỉnh sửa tiêu đề bài báo";
+
+const targetTitle = document.querySelector(".article-content .article-title ");
+
+if (targetTitle) {
+    targetTitle.textContent = "Sinh viên UDU Top 1 sever Hoàng quốc Việt";
+}
