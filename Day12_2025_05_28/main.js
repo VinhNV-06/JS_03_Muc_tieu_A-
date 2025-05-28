77fa7cbaae684b4e79e27784a7eb466c4ceb0f16
VinhNV-06 <vinhnv.ptit@gmail.com>
2025-05-28 21:09:27 +0700

    bai tap day 12

---
+++ Day12_2025_05_28/main.js
@@ -0,0 +1,6 @@
+function changeBg() {
+    const selected = document.getElementById("bgSelect").value; ;
+    document.body.style.backgroundImage = `url('${selected}')` ;
+}
+
+// Hoàn thành day 12 , nội dung : ôn tập lại những kiến thức cũ đã học
